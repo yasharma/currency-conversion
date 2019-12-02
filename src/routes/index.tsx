5c46f229ca76dc0c5bbbca59161e364dfb0ccc2f
Yash Sharma <yashsharma205@gmail.com>
2019-12-02 21:12:32 +0400

    Add React Router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
 import { Switch } from "react-router-dom";
-import { Login } from "../components/Login";
-import { Home } from "../components/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Preview } from "../components/currency-convertor";
-import { Editor } from "../components/Editor";
-import { EditItem } from "../components/EditItem";
 import { ConnectedRouter } from "connected-react-router";
-import { history } from "../store/configureStore";
 
 export const AppRouter = () => {
   return (
-    <ConnectedRouter history={history}>
+    <Router>
       <Switch>
         <PublicRoute path="/" component={Preview} />
       </Switch>
-    </ConnectedRouter>
+    </Router>
   );
 };
